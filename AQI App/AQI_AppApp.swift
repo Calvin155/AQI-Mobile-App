62f605a12ed152fb982ca40a28f4afd88d81c3c1
Calvin <calvinlynch181@outlook.com>
2025-02-05 16:57:01 +0000

    Initial Commit

---
+++ AQI App/AQI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQI_AppApp.swift
+//  AQI App
+//
+//  Created by Calvin Lynch on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AQI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AQI AppTests/AQI_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AQI_AppTests.swift
+//  AQI AppTests
+//
+//  Created by Calvin Lynch on 05/02/2025.
+//
+
+import Testing
+@testable import AQI_App
+
+struct AQI_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
